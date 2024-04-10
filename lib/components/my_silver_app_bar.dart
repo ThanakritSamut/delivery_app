c8fc23b4e7dde5711344b05e17da6a41bff5a28c
Thanakrit Samut <thanakrit_samut@hotmail.com>
2024-04-11 00:29:02 +0700

    Silver App Bar Module
    - Current Location => Alert Dialog
    - Delivery fee
    - Delivery time
    
    == Dummy Data ==

---
+++ lib/components/my_silver_app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MySilverAppBar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+  const MySilverAppBar({super.key, required this.child, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        // cart button
+        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
+      ],
+      backgroundColor: Theme.of(context).colorScheme.background,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text("Sunset Diner"),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:delivery_app/components/my_current_location.dart';
+import 'package:delivery_app/components/my_description_box.dart';
 import 'package:delivery_app/components/my_drawer.dart';
+import 'package:delivery_app/components/my_silver_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,11 +15,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: Text('title'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                // my current location
+                MyCurrentLocation(),
+
+                // description box
+                MyDescriptionBox(),
+              ],
+            ),
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
