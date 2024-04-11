9e399859a8fdf02caa07a56f9b9b3f041dd87e6e
Thanakrit Samut <thanakrit_samut@hotmail.com>
2024-04-11 21:50:23 +0700

    Cart model
    - cart item model
    - addToCart method
    - removeFromCart method
    - get total price
    - get total item count
    - clear cart method

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,3 +1,7 @@
+import 'dart:js_util';
+
+import 'package:collection/collection.dart';
+import 'package:delivery_app/models/cart_item.dart';
 import 'package:delivery_app/models/food.dart';
 import 'package:flutter/material.dart';
 
@@ -353,15 +357,86 @@ class Restaurant extends ChangeNotifier {
   
   */
 
+  // user cart
+  final List<CartItem> _cart = [];
+
   // add to cart
+  void addToCart(Food food, List<Addon> selectedAddons) {
+    // see if there is a cart item already with the same food and selected addons
+    CartItem? cartItem = _cart.firstWhereOrNull((item) {
+      // check if the food items are the same
+      bool isSameFood = item.food == food;
+
+      // check if the list of selected addons are the same
+      bool isSameAddons = ListEquality().equals(
+        item.selectedAddons,
+        selectedAddons,
+      );
+      return isSameFood && isSameAddons;
+    });
+
+    // if item already exists, increase it's quantity
+    if (cartItem != null) {
+      cartItem.quantity++;
+    }
+
+    // otherwise, add a new cart item to the cart
+    else {
+      _cart.add(
+        CartItem(food: food, selectedAddons: selectedAddons),
+      );
+    }
+    notifyListeners();
+  }
 
   // remove from cart
+  void removeFromCart(CartItem cartItem) {
+    int cartIndex = _cart.indexOf(cartItem);
+
+    if (cartItem != -1) {
+      if (_cart[cartIndex].quantity > 1) {
+        _cart[cartIndex].quantity--;
+      } else {
+        _cart.removeAt(cartIndex);
+      }
+    }
+
+    notifyListeners();
+  }
 
   // get total price of cart
+  double getTotalPrice() {
+    double total = 0.0;
+
+    for (CartItem cartItem in _cart) {
+      double itemTotal = cartItem.food.price;
+
+      for (Addon addon in cartItem.selectedAddons) {
+        itemTotal += addon.price;
+      }
+
+      total += itemTotal * cartItem.quantity;
+    }
+
+    return total;
+  }
 
   // get total number of items in cart
+  int getTotalItemCount() {
+    int totalItemCount = 0;
+
+    for (CartItem cartItem in _cart) {
+      totalItemCount += cartItem.quantity;
+    }
+
+    return totalItemCount;
+  }
 
   // clear cart
+  void clearCart() {
+    _cart.clear();
+    notifyListeners();
+  }
 
   /*
 
--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -1,17 +1,19 @@
 import 'package:delivery_app/components/my_button.dart';
 import 'package:delivery_app/models/food.dart';
+import 'package:delivery_app/models/restaurant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class FoodPage extends StatefulWidget {
   final Food food;
-  final Map<Addon, bool> selectAddons = {};
+  final Map<Addon, bool> selectedAddons = {};
 
   FoodPage({super.key, required this.food}) {
     //initialize selected addons to be false
     for (Addon addon in food.availableAddons) {
-      selectAddons[addon] = false;
+      selectedAddons[addon] = false;
     }
   }
 
@@ -20,6 +22,23 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  // method to add to cart
+  void addToCart(Food food, Map<Addon, bool> selectedAddons) {
+    // close the current food page to go back to menu
+    Navigator.pop(context);
+
+    // format the selected addons
+    List<Addon> currentlySelectedAddons = [];
+    for (Addon addon in widget.food.availableAddons) {
+      if (widget.selectedAddons[addon] == true) {
+        currentlySelectedAddons.add(addon);
+      }
+    }
+
+    // add to cart
+    context.read<Restaurant>().addToCart(food, currentlySelectedAddons);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -108,10 +127,10 @@ class _FoodPageState extends State<FoodPage> {
                                   color: Theme.of(context).colorScheme.primary,
                                 ),
                               ),
-                              value: widget.selectAddons[addon],
+                              value: widget.selectedAddons[addon],
                               onChanged: (bool? value) {
                                 setState(() {
-                                  widget.selectAddons[addon] = value!;
+                                  widget.selectedAddons[addon] = value!;
                                 });
                               },
                             );
@@ -125,7 +144,7 @@ class _FoodPageState extends State<FoodPage> {
                 // button => add to cart
                 MyButton(
                   text: "Add to cart",
-                  onTap: () {},
+                  onTap: () => addToCart(widget.food, widget.selectedAddons),
                 ),
                 SizedBox(height: 25),
               ],
