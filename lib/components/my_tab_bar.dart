8d9f41bd62e00419a810157d5030cc081fc6e46c
Thanakrit Samut <thanakrit_samut@hotmail.com>
2024-04-11 16:20:49 +0700

    Tab Bar Module
    - add a tab bar with 3 section => home, setting, person

---
+++ lib/components/my_tab_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyTabBar extends StatelessWidget {
+  final TabController tabController;
+
+  const MyTabBar({super.key, required this.tabController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TabBar(
+        controller: tabController,
+        tabs: [
+          // 1st tab
+          Tab(
+            icon: Icon(Icons.home),
+          ),
+          // 2nd tab
+          Tab(
+            icon: Icon(Icons.settings),
+          ),
+          // 3rd tab
+
+          Tab(
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:delivery_app/components/my_current_location.dart';
 import 'package:delivery_app/components/my_description_box.dart';
 import 'package:delivery_app/components/my_drawer.dart';
 import 'package:delivery_app/components/my_silver_app_bar.dart';
+import 'package:delivery_app/components/my_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,36 +12,67 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  //tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
       body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          MySilverAppBar(
-            title: Text('title'),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                // my current location
-                MyCurrentLocation(),
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                MySilverAppBar(
+                  title: MyTabBar(
+                    tabController: _tabController,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Divider(
+                        indent: 25,
+                        endIndent: 25,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      // my current location
+                      MyCurrentLocation(),
 
-                // description box
-                MyDescriptionBox(),
+                      // description box
+                      MyDescriptionBox(),
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-        ],
-        body: Container(
-          color: Colors.blue,
-        ),
-      ),
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("first"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("second"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("third"),
+              ),
+            ],
+          )),
     );
   }
 }
